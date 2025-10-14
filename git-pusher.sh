e5c96060bd320014218978c8860371a12c49ad96
Martin-Moras <martin.moras@web.de>
2025-10-14 22:34:49 +0200

    ult-pusher.sh test

--- git-pusher.sh
+++ git-pusher.sh
@@ -12,12 +12,11 @@ COMMIT_MESSAGE="$1"
 rm -rf /home/mmoras/Documents/42-projects-git/projects
 cp -r /home/mmoras/Documents/projects /home/mmoras/Documents/42-projects-git
 
-
-# Stage all changes
-git add .
-
-# Commit with the provided message
-git commit -m "$COMMIT_MESSAGE"
-
-# Push to the current branch
-git push
+#Personal repo
+git -C /home/mmoras/Documents/42-projects-git add --a
+git -C /home/mmoras/Documents/42-projects-git commit -m "$COMMIT_MESSAGE"
+git -C /home/mmoras/Documents/42-projects-git push
+#vogosphere repo
+git -C /home/mmoras/Documents/projects add --a
+git -C /home/mmoras/Documents/projects commit -m "$COMMIT_MESSAGE"
+git -C /home/mmoras/Documents/projects push
