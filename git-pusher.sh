5a0e1d34acfdb0cfab6b502186dd2da209c49fc6
Martin-Moras <martin.moras@web.de>
2025-10-14 21:56:41 +0200

    initialization

--- git-pusher.sh
+++ git-pusher.sh
@@ -8,6 +8,12 @@ fi
 
 COMMIT_MESSAGE="$1"
 
+#copy from 42 project
+rm -r /home/mmoras/Documents/42-projects-git/projects
+mkdir /home/mmoras/Documents/42-projects-git/projects
+cp -r /home/mmoras/Documents/projects /home/mmoras/Documents/42-projects-git/projects
+
+
 # Stage all changes
 git add .
 
